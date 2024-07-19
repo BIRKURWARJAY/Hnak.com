fbfb2a777e0b048f1f0dd438aa039fad94ac6309
BIRKURWARJAY <146853234+BIRKURWARJAY@users.noreply.github.com>
2024-07-19 19:57:06 +0530

    add initial files

---
+++ hnak.js
@@ -0,0 +1,9 @@
+function uppercase() {
+   const x = document.getElementById("search");
+   x.value = x.value.toUpperCase();
+}
+
+// document.querySelector("form").addEventListener("submit",function(e){e.preventDefault()})
+// function m() {
+//     if (querySelector("form").addEventListener("onchange",) )
+// }
